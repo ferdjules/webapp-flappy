b1d19137b2a8ed22293e9e153899d09981fe2999
ferdjules <ucabjvo@ucl.ac.uk>
2019-06-25 20:04:08 +0100

    deploymentChanges

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ app.get("/", function(request, response){
 });
 
 app.post('/score', function(request, response){
-    
+
 });
 
-var server = app.listen(8080, function() {
+var server = app.listen((process.env.PORT || 8080), function() {
     var host = server.address().address;
     var port = server.address().port;
 
